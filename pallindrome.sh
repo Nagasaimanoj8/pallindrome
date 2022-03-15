9a50946dd0b48d812b8f230e098b42982e93b688
unknown <nagasaimanoj333@gmail.com>
2022-03-15 17:46:36 +0530

    pallindrome

---
+++ pallindrome.sh
@@ -0,0 +1,16 @@
+echo "enter a nummber"
+read n
+rev=0
+temp=$n;
+while [ $n -ne 0 ]
+do
+	r=`expr $n % 10`
+	rev=$((($rev * 10) +$r))
+	n=`expr $n / 10`
+done
+if [ $temp==$rev ]
+then
+echo "pallindrome"
+else
+echo "not pallindrome"
+fi
